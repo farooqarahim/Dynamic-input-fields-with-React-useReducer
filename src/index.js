b4df83725678743314cd08f2288f77e3c53508e2
Farooq A Rahim <921359+farooqarahim@users.noreply.github.com>
2023-09-28 16:58:57 -0400

    Refactor Dynamic Input Fields Component

--- src/index.js
+++ src/index.js
@@ -1,77 +1,65 @@
 import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
+
+const initialState = [];
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "add":
-      return [...state, action.payload];
+      return [...state, { value: null }];
     case "remove":
-      state.splice(action.payload.index, 1);
-      return [...state];
+      return state.filter((_, index) => index !== action.payload.index);
     case "update":
-      state[action.payload.index]["value"] = action.payload.value;
-      return [...state];
+      return state.map((item, index) =>
+        index === action.payload.index
+          ? { ...item, value: action.payload.value }
+          : item
+      );
     default:
       return state;
   }
 };
 
 const App = () => {
-  let initialState = [
-    {
-      type: "add",
-      payload: { value: null }
-    }
-  ];
-
   const [inputFields, setInputFields] = useReducer(reducer, initialState);
 
   const handleAddMoreFields = () => {
-    setInputFields({
-      type: "add",
-      payload: { value: null }
-    });
+    setInputFields({ type: "add" });
   };
 
-  const handleRemoveField = index => {
-    setInputFields({
-      type: "remove",
-      payload: { index: index }
-    });
+  const handleRemoveField = (index) => {
+    setInputFields({ type: "remove", payload: { index } });
   };
 
-  const handleFieldValues = (i, event) => {
+  const handleFieldValues = (index, event) => {
     setInputFields({
       type: "update",
-      payload: { index: i, value: event.target.value }
+      payload: { index, value: event.target.value }
     });
   };
 
   return (
     <div className="App">
       <h1>Dynamic input fields with React.js / useReducer</h1>
-      {inputFields.map((item, index) => {
-        return (
-          <div key={`${item}-${index}`} className="Wrapper">
-            <input
-              className="InputField"
-              type="text"
-              placeholder="Enter text"
-              value={item.value || ""}
-              onChange={e => handleFieldValues(index, e)}
-            />
-            <button
-              type="button"
-              className="remove-button button"
-              onClick={() => handleRemoveField(index)}
-            >
-              Remove
-            </button>
-          </div>
-        );
-      })}
+      {inputFields.map((item, index) => (
+        <div key={index} className="Wrapper">
+          <input
+            className="InputField"
+            type="text"
+            placeholder="Enter text"
+            value={item.value || ""}
+            onChange={(e) => handleFieldValues(index, e)}
+          />
+          <button
+            type="button"
+            className="remove-button button"
+            onClick={() => handleRemoveField(index)}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
 
       {inputFields.length === 0 && (
         <div className="nothing">Click the button below to add field</div>
@@ -80,7 +68,7 @@ const App = () => {
       <button
         type="button"
         className="add-button button"
-        onClick={() => handleAddMoreFields()}
+        onClick={handleAddMoreFields}
       >
         Add More Input Fields
       </button>
